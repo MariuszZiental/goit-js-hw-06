5abfc164c983c62287257c2de686295c1106e49c
MariuszZiental <122224840+MariuszZiental@users.noreply.github.com>
2023-05-29 21:02:29 +0200

    hw6 js task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+// Pobranie elementów z HTML
+var changeColorButton = document.querySelector(".change-color");
+var colorSpan = document.querySelector(".color");
+
+// Obsługa zdarzenia click na przycisku
+changeColorButton.addEventListener("click", function () {
+  var randomColor = getRandomHexColor();
+
+  // Zmiana koloru tła body
+  document.body.style.backgroundColor = randomColor;
+
+  // Aktualizacja tekstu w elemencie span.color
+  colorSpan.textContent = randomColor;
+});
